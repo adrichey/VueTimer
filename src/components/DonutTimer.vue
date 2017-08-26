0330bcd48d7efff68c1d8b74d9bae936436fb9b9
Alan Richey <alan.richey@fire-engine-red.com>
2017-08-26 18:25:17 -0500

    Initial commit

---
+++ src/components/DonutTimer.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg width="600" height="600"></svg>
+</template>
+
+<script>
+const d3 = require('d3');
+
+export default {
+  name: 'donut-timer',
+  mounted() {
+    // this.#el - is the root element in <template>
+    // in this case it is <svg> tag
+    d3.select(this.$el)
+      .append('circle')
+      .attr('cx', '300')
+      .attr('cy', '300')
+      .attr('r', '300');
+  },
+};
+</script>
+
+<style>
+</style>
